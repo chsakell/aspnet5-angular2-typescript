616c6e7edbe41f9f70270fd998eac6ce7745d2b7
chsakell <chsakell@gmail.com>
2016-02-27 08:50:06 +0200

    Update app to angular2 - beta7

--- src/PhotoGallery/wwwroot/app/app.ts
+++ src/PhotoGallery/wwwroot/app/app.ts
@@ -1,4 +1,6 @@
-﻿import {provide, Component, View} from 'angular2/core';
+﻿///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
+
+import {provide, Component, View} from 'angular2/core';
 import {CORE_DIRECTIVES} from 'angular2/common';
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_BINDINGS, HTTP_PROVIDERS, Headers, RequestOptions, BaseRequestOptions} from 'angular2/http';
