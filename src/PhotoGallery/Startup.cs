2f428a1177494b4f24fd8a1d6321b9268776bc42
chsakell <chsakell@gmail.com>
2016-09-27 23:28:32 +0300

    add Linux & MAC support

--- src/PhotoGallery/Startup.cs
+++ src/PhotoGallery/Startup.cs
@@ -52,9 +52,20 @@ namespace PhotoGallery
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<PhotoGalleryContext>(options =>
-                options.UseSqlServer(Configuration["Data:PhotoGalleryConnection:ConnectionString"]));
+            string sqlConnectionString = Configuration["Data:PhotoGalleryConnection:ConnectionString"];
+            bool useInMemoryProvider = bool.Parse(Configuration["Data:PhotoGalleryConnection:InMemoryProvider"]);
 
+            services.AddDbContext<PhotoGalleryContext>(options => {
+                switch (useInMemoryProvider)
+                {
+                    case true:
+                        options.UseInMemoryDatabase();
+                        break;
+                    default:
+                        options.UseSqlServer(sqlConnectionString);
+                    break;
+                }
+            });
 
             // Repositories
             services.AddScoped<IPhotoRepository, PhotoRepository>();
